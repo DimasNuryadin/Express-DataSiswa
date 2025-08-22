39596b471d79fcd634809c0713027d1f452c69aa
DimasNuryadin <dimasnuryadinn@gmail.com>
2025-08-22 13:22:34 +0700

    done tugas

---
+++ config/db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'siswa',
+});
+module.exports = connection.promise();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,66 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const db = require('../config/db');
+
+/* GET users. */
+router.get('/', async function (req, res, next) {
+  const [rows] = await db.query("SELECT * FROM datasiswa");
+  return res.json({
+    data: rows
+  })
 });
+
+/* POST users. */
+router.post('/', async (req, res) => {
+  const { kode, nama, alamat, tanggal, jurusan } = req.body;
+  if (!kode || !nama || !alamat || !tanggal || !jurusan) {
+    return res.status(400).json({
+      message: "Silahkan isi semua data!!",
+    })
+  }
+
+  const [result] = await db.query("INSERT INTO datasiswa (kode, nama, alamat, tanggal, jurusan) VALUES (?, ?, ?, ?, ?)", [kode, nama, alamat, tanggal, jurusan]);
+
+  return res.json({
+    message: "Data berhasil ditambahkan",
+    id: result.insertId
+  })
+})
+
+/* PUT users. */
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { kode, nama, alamat, tanggal, jurusan } = req.body;
+
+  await db.query("UPDATE datasiswa SET kode=?, nama=?, alamat=?, tanggal=?, jurusan=? WHERE id=?", [kode, nama, alamat, tanggal, jurusan, id]);
+
+  if (!kode || !nama || !alamat || !tanggal || !jurusan) {
+    return res.status(400).json({
+      message: "Silahkan isi semua data!!",
+    })
+  }
+  return res.json({
+    message: "Data berhasil diubah",
+    data: id
+  })
+})
+
+/* DELETE users. */
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({
+      message: "Error, ID tidak ditemukan!"
+    })
+  }
+
+  await db.query("DELETE FROM datasiswa WHERE id=?", [id]);
+  return res.json({
+    data: "Data berhasil dihapus!",
+    id
+  })
+})
 
 module.exports = router;
